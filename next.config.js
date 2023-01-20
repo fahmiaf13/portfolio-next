4d8950c33ac4d08963230112ee5fda41fcd6aea0
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-01-20 09:21:14 +0700

    chore: swap font becoming true

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placekitten.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
