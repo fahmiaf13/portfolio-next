940f92457cfe19dd9aa5e21f2fe9ae4db0441dda
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-01-26 19:29:26 +0700

    fix: footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,6 +3,41 @@ import Logo from "public/icons/ic-logo.svg";
 import { Iconify } from "@/components";
 import Link from "next/link";
 
+const iconFooter = {
+  social: [
+    {
+      icon: "mdi:twitter",
+      link: "https://twitter.com/fahmiaf13",
+    },
+    {
+      icon: "mdi:github",
+      link: "https://github.com/fahmiaf13",
+    },
+    {
+      icon: "mdi:linkedin",
+      link: "https://www.linkedin.com/in/fahmi-achmad-fahrudin",
+    },
+  ],
+  support: [
+    {
+      icon: "file-icons:nextjs",
+      link: "https://nextjs.org/",
+    },
+    {
+      icon: "mdi:tailwind",
+      link: "https://tailwindcss.com/",
+    },
+    {
+      icon: "mdi:language-typescript",
+      link: "https://www.typescriptlang.org/",
+    },
+    {
+      icon: "ion:logo-vercel",
+      link: "https://vercel.com",
+    },
+  ],
+};
+
 export default function Footer() {
   return (
     <section className="bg-milk text-darker h-screen flex items-end">
@@ -10,34 +45,26 @@ export default function Footer() {
         <div className="container py-5 flex flex-col w items-center justify-center gap-3">
           <div className="flex flex-col items-center">
             <Image src={Logo} width={64} alt="logo" />
-            <div className="text-xl font-extrabold tracking-wider">FAHMI ACHMAD FAHRUDIN</div>
+            <div className="text-2xl font-extrabold tracking-wider">FAHMI ACHMAD FAHRUDIN</div>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex gap-2 flex-col items-center">
             <div className="text-lg font-extrabold text-milk/30 tracking-wide">SOCIAL</div>
             <div className="flex gap-2">
-              <Link href="https://github.com/fahmiaf13" className="hover:text-milk/30 duration-300">
-                <Iconify icon="mdi:github" />
-              </Link>
-              <Link href="https://www.linkedin.com/in/fahmi-achmad-fahrudin/" className="hover:text-milk/30 duration-300">
-                <Iconify icon="mdi:linkedin" />
-              </Link>
+              {iconFooter?.social?.map((item, index) => (
+                <Link key={index} href={item.link} className="hover:text-milk/10 duration-300 text-milk/30">
+                  <Iconify icon={item.icon} />
+                </Link>
+              ))}
             </div>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex gap-2 flex-col items-center">
             <div className="text-lg font-extrabold text-milk/30 tracking-wide">WAS BUILT WITH</div>
             <div className="flex gap-2">
-              <Link href="https://nextjs.org/" className="hover:text-milk/30 duration-300">
-                <Iconify icon="file-icons:nextjs" />
-              </Link>
-              <Link href="https://tailwindcss.com/" className="hover:text-milk/30 duration-300">
-                <Iconify icon="mdi:tailwind" />
-              </Link>
-              <Link href="https://www.typescriptlang.org/" className="hover:text-milk/30 duration-300">
-                <Iconify icon="mdi:language-typescript" />
-              </Link>
-              <Link href="https://vercel.com" className="hover:text-milk/30 duration-300">
-                <Iconify icon="ion:logo-vercel" />
-              </Link>
+              {iconFooter?.support?.map((item, index) => (
+                <Link key={index} href={item.link} className="hover:text-milk/10 duration-300 text-milk/30">
+                  <Iconify icon={item.icon} />
+                </Link>
+              ))}
             </div>
           </div>
           <div className="font-extralight text-milk/30 mt-5">Copyright © 2023 - All right reserved</div>
